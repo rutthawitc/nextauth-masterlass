4c347449744084cbd6d8db1c645dcb9dd744be17
rutthawit ch <rutthawit@gmail.com>
2024-05-31 14:53:16 +0700

    -Add Sign Button

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,26 @@
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 
+const poppins = Poppins({ subsets: ["latin"], weight: ["600"] });
 export default function Home() {
   return (
-    <main className='flex h-full flex-col text-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-blue-200 to-blue-800'>
-      <div className='space-y-6'>
-        <h1 className='text-6xl font-semibold text-white drop-shadow-md'>
+    <main className="flex h-full flex-col text-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-blue-200 to-blue-800">
+      <div className="space-y-6">
+        <h1
+          className={cn(
+            "text-6xl font-semibold text-white drop-shadow-md",
+            poppins.className
+          )}
+        >
           🔐Auth
         </h1>
         <p className="text-white text-lg">Simple Authentication service.</p>
+        <div>
+          <Button variant="secondary" size={"lg"}>
+            Sign In
+          </Button>
+        </div>
       </div>
     </main>
   );
